fff4dd5dc9f4a0ca3f9355a46eac2745bc23b784
francastelobranco <francy.cb2@gmail.com>
2024-01-02 20:41:08 -0300

    Inserindo logs

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
@@ -27,11 +29,14 @@ public class AuthenticationController {
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
                                                    @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto){
+        log.debug("POST registerUser userDto recebido {} ", userDto.toString());
         if (userService.existsByUsername(userDto.getUsername())){
+            log.warn("Erro: O usuário com nome {} já está sendo utilizado.", userDto.getUsername());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Erro: O nome do usuário já está sendo utilizado.");
         }
         if (userService.existsByEmail(userDto.getEmail())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Erro: O nome do email já está sendo utilizado.");
+            log.warn("Erro: O email {} já está sendo utilizado.", userDto.getEmail());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Erro: O email já está sendo utilizado.");
         }
 
         UserModel userModel = new UserModel();
@@ -42,7 +47,8 @@ public class AuthenticationController {
         userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
         userService.save(userModel);
+        log.debug("POST registerUser userModel salvo {} ", userModel.toString());
+        log.debug("Usuário salvo com sucesso: {} ", userModel.getUserId());
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
-
     }
 }
--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.ead.authuser.specifications.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,6 +24,7 @@ import java.util.UUID;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/users")
@@ -55,11 +57,14 @@ public class UserController {
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId){
+        log.debug("DELETE deleteUser userId recebido {} ", userId.toString());
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if(!userModelOptional.isPresent()){
             return  ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
         } else{
             userService.delete(userModelOptional.get());
+            log.debug("DELETE deleteUser usuário deletado {} ", userId.toString());
+            log.debug("Usuário deletado com sucesso: {} ", userId.toString());
             return  ResponseEntity.status(HttpStatus.OK).body("Usuário deletado.");
         }
     }
@@ -68,6 +73,7 @@ public class UserController {
     public ResponseEntity<Object> updateUser(@PathVariable(value = "userId") UUID userId,
                                              @RequestBody @Validated(UserDto.UserView.UserPut.class)
                                              @JsonView(UserDto.UserView.UserPut.class) UserDto userDto){
+        log.debug("PUT updateUser userDto recebido {} ", userDto.toString());
         Optional<UserModel> userModelOptional = userService.findById(userId);
 
         if (!userModelOptional.isPresent()){
@@ -79,6 +85,8 @@ public class UserController {
             userModel.setCpf(userDto.getCpf());
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
             userService.save(userModel);
+            log.debug("PUT updateUser userModel salvo {} ", userModel.toString());
+            log.debug("Usuário atualizado com sucesso: {} ", userModel.getUserId());
             return ResponseEntity.status(HttpStatus.OK).body(userModel);
         }
     }
@@ -87,11 +95,13 @@ public class UserController {
     public ResponseEntity<Object> updatePassword(@PathVariable(value = "userId") UUID userId,
                                                  @RequestBody @Validated(UserDto.UserView.PasswordPut.class)
                                                  @JsonView(UserDto.UserView.PasswordPut.class) UserDto userDto){
+        log.debug("PUT updatePassword userDto recebido {} ", userDto.toString());
         Optional<UserModel> userModelOptional = userService.findById(userId);
 
         if (!userModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O usuário não existe");
         } if (!userModelOptional.get().getPassword().equals(userDto.getOldPassword())){
+            log.warn("As senhas não coincidem. userId: {} ", userId.toString());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("As senhas não coincidem");
         }
         else {
@@ -99,6 +109,8 @@ public class UserController {
             userModel.setPassword(userDto.getPassword());
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
             userService.save(userModel);
+            log.debug("PUT updatePassword userModel saved {} ", userModel.toString());
+            log.info("Senha atualizada. userId {} ", userId.toString());
             return ResponseEntity.status(HttpStatus.OK).body("Senha atualizada.");
         }
     }
@@ -107,6 +119,7 @@ public class UserController {
     public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId,
                                               @RequestBody @Validated(UserDto.UserView.ImagePut.class)
                                               @JsonView(UserDto.UserView.ImagePut.class) UserDto userDto){
+        log.debug("PUT updateImage userDto recebido {} ", userDto.toString());
         Optional<UserModel> userModelOptional = userService.findById(userId);
 
         if (!userModelOptional.isPresent()) {
@@ -116,7 +129,10 @@ public class UserController {
             userModel.setImageUrl(userDto.getImageUrl());
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
             userService.save(userModel);
+            log.debug("PUT updateImage userModel salva {} ", userModel.toString());
+            log.info("Image atualizada com sucessp userId {} ", userModel.getUserId());
             return ResponseEntity.status(HttpStatus.OK).body(userModel);
         }
     }
+
 }
